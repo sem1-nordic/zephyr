db6554a3f48db39c4397f5a1744515a21affbc4f
Sean Madigan <sean.madigan@nordicsemi.no>
2022-10-24 16:37:10 +0100

    Bluetooth: Host: init bt_workq and cancel rpa_update work
    Init bt_workq in bt_enable so it can be safely disabled.
    
    Cancel rpa_update work upon disable as it is no longer required.
    
    Signed-off-by: Sean Madigan <sean.madigan@nordicsemi.no>

--- subsys/bluetooth/host/hci_core.c
+++ subsys/bluetooth/host/hci_core.c
@@ -3716,6 +3716,7 @@ int bt_enable(bt_ready_cb_t cb)
 
 #if defined(CONFIG_BT_RECV_WORKQ_BT)
 	/* RX thread */
+	k_work_queue_init(&bt_workq);
 	k_work_queue_start(&bt_workq, rx_thread_stack,
 			   CONFIG_BT_RX_STACK_SIZE,
 			   K_PRIO_COOP(CONFIG_BT_RX_PRIO), NULL);
@@ -3787,6 +3788,10 @@ int bt_disable(void)
 #if defined(CONFIG_BT_EXT_ADV) || defined(CONFIG_BT_PERIPHERAL)
 	bt_adv_reset_adv_pool();
 #endif /* CONFIG_BT_EXT_ADV || CONFIG_BT_PERIPHERAL */
+
+#if defined(CONFIG_BT_PRIVACY)
+	k_work_cancel_delayable(&bt_dev.rpa_update);
+#endif /* CONFIG_BT_PRIVACY */
 
 #if defined(CONFIG_BT_PER_ADV_SYNC)
 	bt_periodic_sync_disable();
